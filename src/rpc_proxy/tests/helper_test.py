837ba071cb796cf6f46cf47a967e06b961939aca
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-11-03 17:31:19 +0300

    test update

--- src/rpc_proxy/tests/helper_test.py
+++ src/rpc_proxy/tests/helper_test.py
@@ -9,7 +9,7 @@ class HelperTestCase(unittest.TestCase):
         pass
 
     def test_route_match_01(self):
-        routes = ["^bridge.get_post$", "^database_api.(.*)", "^condenser_api.(.*)"]
+        routes = ["^bridge.get_post$", "^database_api.(.*)", "^condenser_api.(.*)", "^network_broadcast_api(.*)"]
 
         self.assertEqual(route_match(routes, "condenser_api.get_dynamic_global_properties"), "^condenser_api.(.*)")
 
@@ -18,3 +18,5 @@ class HelperTestCase(unittest.TestCase):
         self.assertEqual(route_match(routes, "bridge.get_post"), "^bridge.get_post$")
 
         self.assertEqual(route_match(routes, "unknown_api.get_data"), None)
+
+        self.assertEqual(route_match(routes, "network_broadcast_api.broadcast_block"), "^network_broadcast_api(.*)")
