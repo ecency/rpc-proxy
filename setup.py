747860ee8b7cb400a71a47791cb579d18f95a009
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-11-10 14:40:32 +0300

    setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+assert sys.version_info[0] == 3 and sys.version_info[1] >= 5, "rpc_proxy requires Python 3.6 or newer"
+
+setup(
+    name='rpc_proxy',
+    version='0.0.1',
+    description='rpc proxy',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=[
+        'sanic',
+        'requests',
+        'requests',
+        'aiocache'
+    ],
+    entry_points={
+        'console_scripts': [
+            'rpc_proxy=rpc_proxy.app:main'
+        ]
+    })
