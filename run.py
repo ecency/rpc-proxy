03976a7d4d5ac70a8dac61eef2b7dc4fce71d009
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-11-30 23:00:20 +0300

    code correction

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 
 assert sys.version_info[0] == 3 and sys.version_info[1] >= 6, "Requires Python 3.6 or newer"
 
-os.sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
 
 def main():
