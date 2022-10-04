651eca4f74f0dff4ca61477309df0c265117eca2
feruzm <fmiboy@gmail.com>
2022-10-04 15:05:21 +0500

    define package versions

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ assert sys.version_info[0] == 3 and sys.version_info[1] >= 6, "rpc_proxy require
 
 setup(
     name='rpc_proxy',
-    version='0.0.8',
+    version='0.0.9',
     description='rpc proxy',
     long_description=open('README.md').read(),
     packages=find_packages(),
     install_requires=[
-        'sanic',
-        'sanic_cors',
-        'websockets',
-        'requests',
-        'aiocache',
+        'sanic==20.12.4',
+        'sanic_cors==0.10.0.post3',
+        'websockets==8.1',
+        'requests==2.26.0',
+        'aiocache==0.11.1',
         'aioredis>=1,<2'
     ],
     entry_points={
